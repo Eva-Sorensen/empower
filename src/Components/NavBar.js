103a4243ba64be5316fd7fc77e8bbd7033adb39f
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2022-07-10 11:01:55 +1200

    format nav (#25)

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
+import style from "../css/NavBar.module.css";
 
 export default function NavBar() {
+  // var pathname = window.location.pathname
+  var pathname = "/"
   return (
-    <ul className="mainSection">
-      <Link to="About" className="listLink">
-        <li className="listItem">About</li>
-      </Link>
-      <Link to="" className="listLink">
-        <li className="listItem">Home</li>
-      </Link>
-    </ul>
+    <nav className={style.slide_in}>
+      <div
+        className={style.navbar_container}
+      >
+        <ul>
+          <Link to="" className={style.nav_link}>
+            <li className={`${pathname === "/" ? style.active : ""}`}>
+              Empower
+            </li>
+          </Link>
+          <Link to="about" className={style.nav_link}>
+            <li className={`${pathname === "/about" ? style.active : ""}`}>
+              About
+            </li>
+          </Link>
+          <Link to="meetups" className={style.nav_link}>
+            <li className={`${pathname === "/meetups" ? style.active : ""}`}>
+              Meet ups
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
   );
 }
