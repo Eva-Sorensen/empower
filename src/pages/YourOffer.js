9130ee09a03db9feac904d3f26481d4985345691
Eva-Sorensen <eva.elaine.sorensen@gmail.com>
2022-07-09 23:53:25 +1200

    finished

---
+++ src/pages/YourOffer.js
@@ -0,0 +1,33 @@
+import style from "../css/YourOffer.module.css";
+
+export default function Signup() {
+  return (
+    <div className={style.background}>
+      <h1 className={style.mainHeader}>YOUR OFFERS</h1>
+      <div className={style.offersSection}>
+        <div className={style.offer}>
+          <h2 className={style.offerHeader}>Transport</h2>
+          <p className={style.info}>Name: Jane Doe</p>
+          <p className={style.info}>Email: janedoe4@gmail.com</p>
+          <p className={style.info}>Location: 20 Auckand City, Auckland</p>
+          <p className={style.openDate}>Open Since: 20/02/2022</p>
+          <div className={style.buttons}>
+            <button className={style.edit}>edit</button>
+            <button className={style.close}>close offer</button>
+          </div>
+        </div>
+        <div className={style.offer}>
+          <h2 className={style.offerHeader}>Advice</h2>
+          <p className={style.info}>Name: Jane Doe</p>
+          <p className={style.info}>Email: janedoe4@gmail.com</p>
+          <p className={style.info}>Location: n/a</p>
+          <p className={style.openDate}>Open Since: 10/07/2022</p>
+          <div className={style.buttons}>
+            <button className={style.edit}>edit</button>
+            <button className={style.close}>close offer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
