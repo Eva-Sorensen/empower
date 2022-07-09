2c9476ea7e9172c945b6112ab4aa604cb70d65a8
Kimberley <kim@evans-parker.com>
2022-07-09 23:20:20 +1200

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,13 @@
 export default function About() {
-  return <h1>About Page!</h1>;
+  return <div className="container fade left">
+      <h2>About Us</h2>
+      <p>Why are we here?</p>
+      <p>We want to give woman the power of independence.</p>
+      <p>We know not all woman have the same freedoms most of us take for granted.  
+        Some woman feel oppressed or controlled by those they live with, 
+        whether it's by physical means, financial or mental.  
+        We want to give woman the power to support themselves in everything they do.  
+      </p>
+      <p>Through Empower, you can talk to other women, make friends, provide support and find your own freedom in the world</p>
+    </div>;
 }
