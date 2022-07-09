12ad87cc993a2c84fed12549706a0b9e722b3aca
Kimberley <kim@evans-parker.com>
2022-07-09 23:59:03 +1200

    Add button

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import style from "../css/About.module.css";
+
 export default function About() {
   return <div className="container fade left">
       <h2>About Us</h2>
@@ -9,5 +12,16 @@ export default function About() {
         We want to give woman the power to support themselves in everything they do.  
       </p>
       <p>Through Empower, you can talk to other women, make friends, provide support and find your own freedom in the world</p>
+      <br />
+      <div className={style.link_button}>
+        <div class={style.link_wrapper}>
+          <a href="#">Join the cause!</a>
+          <div class={style.icon}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
+              <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>;
 }
